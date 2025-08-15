41af7ee90edb6dc666b30c0f06c373e6e416fcc2
Maciej.Jakubiak <maciej.z.jakubiak.civ@army.mil>
2025-08-15 14:39:59 -0500

    first

---
+++ run.sh
@@ -0,0 +1,11 @@
+mkdir -p jellyfin/config jellyfin/media
+mkdir -p radarr/config radarr/downloads radarr/movies
+mkdir -p sonarr/config sonarr/downloads sonarr/tv
+mkdir -p prowlarr/config
+mkdir -p jellyseerr/config
+docker-compose up -d
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.7.0/aio/deploy/recommended.yaml
+# kubectl create serviceaccount dashboard-admin -n kubernetes-dashboard
+# kubectl create clusterrolebinding dashboard-admin --clusterrole=cluster-admin --serviceaccount=kubernetes-dashboard:dashboard-admin
+# kubectl describe secret $(kubectl get secrets -n kubernetes-dashboard | grep dashboard-admin | awk '{print $1}') -n kubernetes-dashboard
+# kubectl proxy
